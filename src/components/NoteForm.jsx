60db432d3b8b9cceb35d815ef48282155632984d
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-05-16 13:37:29 +0800

    Testing the form

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -15,7 +15,7 @@ const NoteForm = ({ createNote }) => {
   }
 
   return (
-    <div>
+    <div className="formDiv">
       <h2>Create a new note</h2>
       <form onSubmit={onSubmit}>
         <input
